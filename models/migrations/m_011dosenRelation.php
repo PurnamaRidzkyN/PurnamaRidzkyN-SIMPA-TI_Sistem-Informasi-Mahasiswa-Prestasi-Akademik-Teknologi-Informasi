2574fd4ca236540efa1bec633ab3cba228c1432b
PurnamaRidzkyN <purnamanugraha492@gmail.com>
2024-12-03 16:28:14 +0700

    dosen

--- models/migrations/m_011dosenRelation.php
+++ models/migrations/m_011dosenRelation.php
@@ -10,14 +10,14 @@ class m_017AdminRelation implements BaseMigration
     public function up(): array
     {
         return Schema::alterTable("dosen", function (Blueprint $table) {
-            $table->alterAddForeignKey("user_id", "user", "id", "Fk_user_id_admin");
+            $table->alterAddForeignKey("user_id", "user", "id", "Fk_user_id_dosen");
         });
     }
 
     public function down(): array
     {
       return Schema::alterTable("dosen", function (Blueprint $table) {
-          $table->alterDropConstraint("FK_user_id_admin");
+          $table->alterDropConstraint("FK_user_id_dosen");
       });
     }
 }
