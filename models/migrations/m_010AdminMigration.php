aa45633a656e0bc81c3daf109a7a9d6ee883383e
PurnamaRidzkyN <purnamanugraha492@gmail.com>
2024-12-03 15:40:17 +0700

    feat add admin

---
+++ models/migrations/m_010AdminMigration.php
@@ -0,0 +1,24 @@
+<?php
+
+use app\cores\Blueprint;
+use app\cores\Schema;
+use app\models\BaseMigration;
+
+class m_004DosenMigration implements BaseMigration
+{
+    public function up(): array
+    {
+        return Schema::createTableIfNotExist("admin", function (Blueprint $table) {
+            $table->int("id");
+            $table->int("user_id"); // Sesuai SQL, bisa null
+            $table->string("nip");
+
+            $table->primary("id");
+        });
+    }
+
+    public function down(): array
+    {
+        return Schema::dropTableIfExist("admin");
+    }
+}
