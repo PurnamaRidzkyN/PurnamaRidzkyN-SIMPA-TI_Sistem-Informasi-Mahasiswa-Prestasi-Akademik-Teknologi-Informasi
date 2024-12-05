07b60dca091dc0d6aa1e6188f7443722db5b556d
PurnamaRidzkyN <purnamanugraha492@gmail.com>
2024-12-05 22:09:53 +0700

    feat

--- models/database/users/Admin.php
+++ models/database/users/Admin.php
@@ -1,26 +1,36 @@
 <?php
 
-namespace app\models\database\users;
 use app\cores\Blueprint;
 use app\cores\Schema;
 use app\models\BaseModel;
-class Admin
+
+class Admin extends BaseModel
 {
     public const TABLE = "admin";
     public const ID = "id";
-    public const ID_USER ="id_user";
+    public const ID_USER = "id_user";
     public const NIP = "nip";
-    public const NAME = "name";
+    public const NAMA = "nama";
+    public const FOTO = "foto";
     public const EMAIL = "email";
 
     public static function insert(array $data): array
     {
         return Schema::insertInto(self::TABLE, function (Blueprint $table) use ($data) {
-            $table->insert([self::ID,self::NIP,self::EMAIL], $data);
+            $table->insert([self::ID,self::ID_USER,self::NIP,self::NAMA,self::FOTO,self::EMAIL], $data);
             
-        });   
-        
+        });
     }
-    
+
+    public static function deleteAll(): array
+    {
+        return Schema::deleteFrom(self::TABLE);
+    }
+    public static function findEmail($email) : array 
+    {
+        return Schema::selectWhereFrom(self::TABLE, function (Blueprint $table) use ($email) {
+            $table->selectWhere(["email" => $email], [self::ID,self::ID_USER,self::NIP,self::EMAIL]);
+        });   
+    }
     
 }
