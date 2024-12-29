077adfec9ce19f7cb0a532e14b2c85e050e183d6
PurnamaRidzkyN <purnamanugraha492@gmail.com>
2024-12-29 20:44:50 +0700

    ffeat managementdata

---
+++ controllers/ManagementData.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\controllers;
+
+use app\cores\Request;
+use app\cores\Response;
+use app\helpers\Dump;
+use app\models\database\logData\LogData;
+use app\models\database\users\Admin;
+use app\models\database\users\Dosen;
+use app\models\database\users\Mahasiswa;
+
+class ManagementData extends BaseController
+{
+    public function renderManagementData(Request $req, Response $res): void
+    {
+        $body = $req->body();
+        $log = LogData::logDataDisplay()["result"];
+        $admin = Admin::displayAdmin()["result"];
+        $mahasiswa = Mahasiswa::displayMahasiswa()["result"];
+        $dosen = Dosen::displayDosen()["result"];
+
+        $data = ["data"=>$body,"admin"=> $admin,"mahasiswa"=>$mahasiswa,"dosen"=>$dosen,"log data"=>$log];
+        $this->view("Dashboard/admin/manajemenData/manajemenData", "Manajemen Data", $data);
+    }
+}
